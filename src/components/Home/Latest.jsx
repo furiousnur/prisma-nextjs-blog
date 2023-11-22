c82d5e8139d09bb98c833a66374e091a40781deb
Nur <furiousnur@gmail.com>
2023-11-22 09:50:57 +0600

    home page category list design

--- src/components/Home/Latest.jsx
+++ src/components/Home/Latest.jsx
@@ -6,142 +6,35 @@ import Subscribers from "@/components/Subscribers/Subscribers";
 const Latest = () => {
     return (
         <div className="container mt-5">
-            <h5>LATEST</h5>
+            <h5>Browse items by category</h5>
             <hr className=""/>
            <div className="row">
-               <div className="col-md-9 col-lg-9 col-sm-12 col-12 px-3">
+               <div className="col-md-12 col-lg-12 col-sm-12 col-12 px-3">
                    <div className="row">
-                       <Fade className="p-2 col-md-4">
-                           <div className="card bg-white shadow-sm">
-                               <img className="card-img-top" src="https://www.aljazeera.com/wp-content/uploads/2023/04/2023-04-05T034020Z_890746073_RC2380AMCMR6_RTRMADP_3_ISRAEL-PALESTINIANS-VIOLENCE-ALAQSA-1.jpg" className="card-img-top" alt="News Image"/>
-                               <div className="card-body">
-                                   <h6 className="card-title">Sample News Headline orders partial embassy</h6>
-                                   <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard </p>
-                                   <p className="my-2 fw-bold p-0"><i className="bi bi-clock"></i> 3 Days Ago</p>
-                                   <Link href="#" className="btn mt-2 btn-sm btn-outline-danger">Read More</Link>
+                       <div className="p-2 col-md-2">
+                           <a className="flex items-center text-center">
+                               <div className="col-md-6 ">
+                                   <i className="bi bi-phone"></i>
                                </div>
-                           </div>
-                       </Fade>
-                       <Fade className="p-2 col-md-4">
-                           <div className="card bg-white shadow-sm">
-                               <img className="card-img-top" src="https://www.aljazeera.com/wp-content/uploads/2023/04/2023-04-05T034020Z_890746073_RC2380AMCMR6_RTRMADP_3_ISRAEL-PALESTINIANS-VIOLENCE-ALAQSA-1.jpg" className="card-img-top" alt="News Image"/>
-                               <div className="card-body">
-                                   <h6 className="card-title">Sample News Headline orders partial embassy</h6>
-                                   <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard </p>
-                                   <p className="my-2 fw-bold p-0"><i className="bi bi-clock"></i> 3 Days Ago</p>
-                                   <Link href="#" className="btn mt-2 btn-sm btn-outline-danger">Read More</Link>
+                               <div className="col-md-6">
+                                   <h6 className="card-title text-right">Mobiles</h6>
+                                   <p>90,104 ads</p>
                                </div>
-                           </div>
-                       </Fade>
-                       <Fade className="p-2 col-md-4">
-                           <div className="card bg-white shadow-sm">
-                               <img className="card-img-top" src="https://www.aljazeera.com/wp-content/uploads/2023/04/2023-04-05T034020Z_890746073_RC2380AMCMR6_RTRMADP_3_ISRAEL-PALESTINIANS-VIOLENCE-ALAQSA-1.jpg" className="card-img-top" alt="News Image"/>
-                               <div className="card-body">
-                                   <h6 className="card-title">Sample News Headline orders partial embassy</h6>
-                                   <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard </p>
-                                   <p className="my-2 fw-bold p-0"><i className="bi bi-clock"></i> 3 Days Ago</p>
-                                   <Link href="#" className="btn mt-2 btn-sm btn-outline-danger">Read More</Link>
-                               </div>
-                           </div>
-                       </Fade>
-                       <Fade className="p-2 col-md-4">
-                           <div className="card bg-white shadow-sm">
-                               <img className="card-img-top" src="https://www.aljazeera.com/wp-content/uploads/2023/04/2023-04-05T034020Z_890746073_RC2380AMCMR6_RTRMADP_3_ISRAEL-PALESTINIANS-VIOLENCE-ALAQSA-1.jpg"  alt="News Image"/>
-                               <div className="card-body">
-                                   <h6 className="card-title">Sample News Headline orders partial embassy</h6>
-                                   <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard </p>
-                                   <p className="my-2 fw-bold p-0"><i className="bi bi-clock"></i> 3 Days Ago</p>
-                                   <Link href="#" className="btn mt-2 btn-sm btn-outline-danger">Read More</Link>
-                               </div>
-                           </div>
-                       </Fade>
-                       <Fade className="p-2 col-md-4">
-                           <div className="card bg-white shadow-sm">
-                               <img className="card-img-top" src="https://www.aljazeera.com/wp-content/uploads/2023/04/2023-04-05T034020Z_890746073_RC2380AMCMR6_RTRMADP_3_ISRAEL-PALESTINIANS-VIOLENCE-ALAQSA-1.jpg"  alt="News Image"/>
-                               <div className="card-body">
-                                   <h6 className="card-title">Sample News Headline orders partial embassy</h6>
-                                   <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard </p>
-                                   <p className="my-2 fw-bold p-0"><i className="bi bi-clock"></i> 3 Days Ago</p>
-                                   <Link href="#" className="btn mt-2 btn-sm btn-outline-danger">Read More</Link>
-                               </div>
-                           </div>
-                       </Fade>
-                       <Fade className="p-2 col-md-4">
-                           <div className="card bg-white shadow-sm">
-                               <img className="card-img-top" src="https://www.aljazeera.com/wp-content/uploads/2023/04/2023-04-05T034020Z_890746073_RC2380AMCMR6_RTRMADP_3_ISRAEL-PALESTINIANS-VIOLENCE-ALAQSA-1.jpg"  alt="News Image"/>
-                               <div className="card-body">
-                                   <h6 className="card-title">Sample News Headline orders partial embassy</h6>
-                                   <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard </p>
-                                   <p className="my-2 fw-bold p-0"><i className="bi bi-clock"></i> 3 Days Ago</p>
-                                   <Link href="#" className="btn mt-2 btn-sm btn-outline-danger">Read More</Link>
-                               </div>
-                           </div>
-                       </Fade>
-                   </div>
-               </div>
-
-               <div  className="col-md-3 col-lg-3 col-sm-12 col-12 px-3">
-                   <div className="row">
-                       <div className="bg-dark mt-2 rounded-1 text-white p-2">
-                           <span className="p-1">POPULAR</span>
+                           </a>
                        </div>
-
-                       <Fade className="col-12 py-1 px-0">
-                           <Link href="" className="card bg-white shadow-sm">
-                               <div className="row">
-                                   <div className="col-md-5">
-                                       <img className=" rounded-start-1 w-100 h-100" src="https://www.newarab.com/sites/default/files/1239992430.jpeg"  alt="News Image"/>
-                                   </div>
-                                   <div className="col-md-7 p-3">
-                                       <h6>Sample News Headline orders partial embassy</h6>
-                                   </div>
+                       <div className="p-2 col-md-2">
+                           <a className="flex items-center text-center">
+                               <div className="col-md-6 ">
+                                   <i className="bi bi-phone"></i>
                                </div>
-                           </Link>
-                       </Fade>
-                       <Fade className="col-12 py-1 px-0">
-                           <Link href="" className="card bg-white shadow-sm">
-                               <div className="row">
-                                   <div className="col-md-5">
-                                       <img className=" rounded-start-1 w-100 h-100" src="https://www.newarab.com/sites/default/files/1239992430.jpeg"  alt="News Image"/>
-                                   </div>
-                                   <div className="col-md-7 p-3">
-                                       <h6>Sample News Headline orders partial embassy</h6>
-                                   </div>
+                               <div className="col-md-6">
+                                   <h6 className="card-title text-right">Mobiles</h6>
+                                   <p>90,104 ads</p>
                                </div>
-                           </Link>
-                       </Fade>
-                       <Fade className="col-12 py-1 px-0">
-                           <Link href="" className="card bg-white shadow-sm">
-                               <div className="row">
-                                   <div className="col-md-5">
-                                       <img className=" rounded-start-1 w-100 h-100" src="https://www.newarab.com/sites/default/files/1239992430.jpeg"  alt="News Image"/>
-                                   </div>
-                                   <div className="col-md-7 p-3">
-                                       <h6>Sample News Headline orders partial embassy</h6>
-                                   </div>
-                               </div>
-                           </Link>
-                       </Fade>
-                       <Fade className="col-12 py-1 px-0">
-                           <Link href="" className="card bg-white shadow-sm">
-                               <div className="row">
-                                   <div className="col-md-5">
-                                       <img className=" rounded-start-1 w-100 h-100" src="https://www.newarab.com/sites/default/files/1239992430.jpeg"  alt="News Image"/>
-                                   </div>
-                                   <div className="col-md-7 p-3">
-                                       <h6>Sample News Headline orders partial embassy</h6>
-                                   </div>
-                               </div>
-                           </Link>
-                       </Fade>
-                       <div className="bg-dark mt-2 rounded-1 text-white p-2">
-                           <span className="p-1">SUBSCRIBE</span>
-                       </div>
-                       <div className="col-12 py-1 px-0">
-                            <Subscribers/>
+                           </a>
                        </div>
                    </div>
-               </div>
+               </div> 
            </div> 
         </div>
     );
